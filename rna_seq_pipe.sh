472935cd03bb5488c3b1d5ea05b46f316ef69d5c
Joe <burkej24@msu.edu>
2020-01-27 14:32:45 -0500

    upped threads on sort

--- rna_seq_pipe.sh
+++ rna_seq_pipe.sh
@@ -25,7 +25,7 @@ cutadapt -g AATGATACGGCGACCACCGAGATCTACACTCTTTCCCTACACGACGCTCTTCCGATCT -a AGATCG
 -A AGATCGGAAGAGCGTCGTGTAGGGAAAGAGTGTAGATCTCGGTGGTCGCCGTATCATT -f fastq -n 2 -m 100 -q 10 -j 4 -o $sra.R1.fastq -p $sra.R2.fastq $sra*.fastq  > cutadaptlog.txt
 fastqc $sra.*
 hisat2-build -f reference$sra.fa $sra.reference
-hisat2 -x $sra.reference -1 $sra.R1.fastq -2 $sra.R2.fastq  -q | samtools view -b -@ 2| samtools sort -O bam -o $sra.sorted.bam
+hisat2 -x $sra.reference -1 $sra.R1.fastq -2 $sra.R2.fastq  -q | samtools view -b -@ 2| samtools sort -@ 2 -O bam -o $sra.sorted.bam
 samtools index $sra.sorted.bam
 samtools idxstats $sra.sorted.bam > idxstats.txt
 samtools flagstat $sra.sorted.bam > flagstats.txt
