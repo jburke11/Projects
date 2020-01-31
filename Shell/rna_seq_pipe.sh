200a742e15feb7bfc08b76dfc160b29090804e04
Joe <burkej24@msu.edu>
2020-01-31 08:00:28 -0500

    modified:   rna_seq_pipe.sh

--- Shell/rna_seq_pipe.sh
+++ Shell/rna_seq_pipe.sh
@@ -15,13 +15,13 @@ prefetch $sra
 fastq-dump --split-files --split-spot -F $sra
 fastqc $sra*
 cutadapt -g AATGATACGGCGACCACCGAGATCTACACTCTTTCCCTACACGACGCTCTTCCGATCT -a AGATCGGAAGAGCACACGTCTGAACTCCAGTCACNNNNNNATCTCGTATGCCGTCTTCTGCTTG -G CAAGCAGAAGACGGCATACGAGATNNNNNNGTGACTGGAGTTCAGACGTGTGCTCTTCCGATCT \
--A AGATCGGAAGAGCGTCGTGTAGGGAAAGAGTGTAGATCTCGGTGGTCGCCGTATCATT -f fastq -n 2 -m 100 -q 10 -j 4 -o $sra.R1.fastq -p $sra.R2.fastq $sra*.fastq  > cutadaptlog.txt
+-A AGATCGGAAGAGCGTCGTGTAGGGAAAGAGTGTAGATCTCGGTGGTCGCCGTATCATT -f fastq -n 2 -m 100 -q 10 -j 4 -o $sra.R1.fastq -p $sra.R2.fastq $sra*.fastq  > $sra.cutadaptlog.txt
 fastqc $sra.*
 hisat2-build -f reference$sra.fa $sra.reference
 hisat2 -x $sra.reference -1 $sra.R1.fastq -2 $sra.R2.fastq  -q | samtools view -b -@ 2| samtools sort -@ 2 -O bam -o $sra.sorted.bam
 samtools index $sra.sorted.bam
-samtools idxstats $sra.sorted.bam > idxstats.txt
-samtools flagstat $sra.sorted.bam > flagstats.txt
+samtools idxstats $sra.sorted.bam > $sra.idxstats.txt
+samtools flagstat $sra.sorted.bam > $sra.flagstats.txt
 }
 
 function unload_modules {   # unloads all modules used
