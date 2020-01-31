2044563e31435f69bf087e254d17f3f3ffb6ab25
Joe <burkej24@msu.edu>
2020-01-31 10:39:25 -0500

    modified:   pipe.sh

--- Shell/rna_seq_pipe.sh
+++ Shell/rna_seq_pipe.sh
@@ -17,6 +17,7 @@ fastqc $sra*
 cutadapt -g AATGATACGGCGACCACCGAGATCTACACTCTTTCCCTACACGACGCTCTTCCGATCT -a AGATCGGAAGAGCACACGTCTGAACTCCAGTCACNNNNNNATCTCGTATGCCGTCTTCTGCTTG -G CAAGCAGAAGACGGCATACGAGATNNNNNNGTGACTGGAGTTCAGACGTGTGCTCTTCCGATCT \
 -A AGATCGGAAGAGCGTCGTGTAGGGAAAGAGTGTAGATCTCGGTGGTCGCCGTATCATT -f fastq -n 2 -m 100 -q 10 -j 4 -o $sra.R1.fastq -p $sra.R2.fastq $sra*.fastq  > $sra.cutadaptlog.txt
 fastqc $sra.R*
+echo "entering pipeline"
 (hisat2 -x reference -1 $sra.R1.fastq -2 $sra.R2.fastq  -q ; >&2 echo "hisat done") | (samtools view -b -@ 2 ; >&2 echo "sam converted to bam") | (samtools sort -@ 2 -O bam -o $sra.sorted.bam ; >&2 echo "sort done")
 samtools index $sra.sorted.bam
 echo "index done"
