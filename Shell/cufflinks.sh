78f275d922d463049984f32f90ad350190e9ac0e
Joe <burkej24@msu.edu>
2020-02-07 11:24:45 -0500

    new file:   cufflinks.sh

---
+++ Shell/cufflinks.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+module load cufflinks
+
+while getopts "b:g:h" opt; do   #fetches command line arguments and creates neccesary vars
+  case $opt in
+    b) bam_input=$OPTARG;;
+    r) wget -O reference.gff $OPTARG;;
+    h) echo "[-b for bam file][-g for gff file] [-h for help]"
+        exit 1;;
+  esac
+done
+
+
+cufflinks -I 5000 -g reference.gff $sra_input
