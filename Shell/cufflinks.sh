b0d7ec57e9ca8d7ad5806d04cd0b1bf45de686b6
jburke11 <burkej24@msu.edu>
2020-02-28 07:42:00 -0500

    Changes to be committed:

--- Shell/cufflinks.sh
+++ Shell/cufflinks.sh
@@ -1,15 +1,6 @@
 #!/bin/bash
-module load cufflinks
+cufflinks -I 5000 -g reference.gff sra's
 
-while getopts "b:g:h" opt; do   #fetches command line arguments and creates neccesary vars
-  case $opt in
-    b) bam_input=$OPTARG;;
-    r) wget -O reference.gff $OPTARG;;
-    h) echo "[-b for bam file][-g for gff file] [-h for help]"
-        exit 1;;
-  esac
-done
+touch assembly_list.txt
 
-
-cufflinks -I 5000 -g reference.gff $sra_input
-echo "cufflinks done"
+cuffmerge -p 2 assembly_list
--- bioinformatics/cuffdiff_to_DESeq2.py
+++ bioinformatics/cuffdiff_to_DESeq2.py
@@ -6,15 +6,13 @@ Created on Mon Feb 17 13:35:14 2020
 @author: burkej24
 """
 
-fp = open("/Users/burkej24/Desktop/genes.count_tracking", "r")
+fp = open("/Users/burkej24/Desktop/De/genes.count_tracking", "r")
 new1= open ("/Users/burkej24/Desktop/DESeq.csv", "w")
 
 for line in fp :
     list1 = line.rstrip().split()
     if list1[0] == "tracking_id" :
         print(list1[0] , ',' , list1[1] , ',' , list1[6], ',' , list1[11], file=new1)
-    elif list1[0][0] != 'L' :
-        pass
     else:
          print(list1[0] , ',' , int(float(list1[1])) , ',' , int(float(list1[6])), ',' , int(float(list1[11])), file=new1)
 print ('\n', file=new1)
